e68b1aedf6875c7b3a2360a4114a8a5ccc1174ff
gustav0-gg <gugugarciaaa+github@gmail.com>
2025-05-13 00:47:57 -0300

    FIX - Arrumando bug vercel

--- src/api/index.py
+++ src/api/index.py
@@ -1,11 +1,13 @@
 import os
 from flask import Flask, render_template
 
-base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-static_dir = os.path.join(base_dir, 'static')
-template_dir = os.path.join(base_dir, 'templates')
 
-app = Flask(__name__, static_folder=static_dir, template_folder=template_dir)
+base_dir = os.path.dirname(os.path.abspath(__file__))
+
+static_folder = os.path.join(base_dir, '..', 'static')
+template_folder = os.path.join(base_dir, '..', 'templates')
+
+app = Flask(__name__, static_folder=static_folder, template_folder=template_folder)
 
 @app.route('/')
 def homepage():
@@ -14,6 +16,5 @@ def homepage():
 def handler(environ, start_response):
     return app(environ, start_response)
 
-
-if __name__ == '__main__' :
+if __name__ == '__main__':
     app.run(debug=True)
