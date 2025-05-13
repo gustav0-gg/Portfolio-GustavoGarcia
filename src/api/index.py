aca49dabae21c199bafc826b84aae70c3ce6dd28
gustav0-gg <gugugarciaaa+github@gmail.com>
2025-05-13 00:43:03 -0300

    FIX - Arrumando caminho das pastas no arquivo py

--- src/api/index.py
+++ src/api/index.py
@@ -1,6 +1,11 @@
+import os
 from flask import Flask, render_template
 
-app = Flask(__name__, static_folder="../static", template_folder="../templates")
+base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+static_dir = os.path.join(base_dir, 'static')
+template_dir = os.path.join(base_dir, 'templates')
+
+app = Flask(__name__, static_folder=static_dir, template_folder=template_dir)
 
 @app.route('/')
 def homepage():
