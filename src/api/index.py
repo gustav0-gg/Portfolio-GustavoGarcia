55e594cb577b4662ddfbc4b2d928a06083e2c52f
gustav0-gg <gugugarciaaa+github@gmail.com>
2025-05-13 01:05:49 -0300

    FIX - Ajuste Flask

--- src/api/index.py
+++ src/api/index.py
@@ -13,12 +13,5 @@ app = Flask(__name__, static_folder=static_folder, template_folder=template_fold
 def homepage():
     return render_template('portfolio.html')
 
-@app.errorhandler(500)
-def server_error(e):
-    return f"<h1>Erro Interno</h1><p>{e}</p>", 500
-
-def handler(environ, start_response):
-    return app(environ, start_response)
-
 if __name__ == '__main__':
     app.run(debug=True)
