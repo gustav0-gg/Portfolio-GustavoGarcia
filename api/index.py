f226970eb70f5b08ccf65a66cf1bf1ae165cfd8d
gustav0-gg <gugugarciaaa+github@gmail.com>
2025-05-13 00:25:08 -0300

    FIX - Corrigindo problema com Vercel

--- api/index.py
+++ api/index.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="../static", template_folder="../templates")
 
 @app.route('/')
 def homepage():
     return render_template('portfolio.html')
+
+def handler(environ, start_response):
+    return app(environ, start_response)
 
 
 if __name__ == '__main__' :
